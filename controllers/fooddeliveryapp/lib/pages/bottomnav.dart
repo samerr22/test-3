3753fc21a39d5587ee47042473655bfe4f39250f
sameeera <laksameera5@gmail.com>
2024-07-30 15:42:42 +0530

    done

--- controllers/fooddeliveryapp/lib/pages/bottomnav.dart
+++ controllers/fooddeliveryapp/lib/pages/bottomnav.dart
@@ -1,42 +1,55 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';  // Import Firebase Auth
 import 'package:fooddeliveryapp/pages/home.dart';
 import 'package:fooddeliveryapp/pages/order.dart';
 import 'package:fooddeliveryapp/pages/profile.dart';
-import 'package:fooddeliveryapp/pages/wallet.dart';
+import 'package:fooddeliveryapp/pages/videoupload.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+  // Adjust the import path based on your file structure
 
 class BottomNav extends StatefulWidget {
-  const BottomNav({super.key});
+  const BottomNav({Key? key}) : super(key: key);
 
   @override
   State<BottomNav> createState() => _BottomNavState();
 }
 
 class _BottomNavState extends State<BottomNav> {
-  int currentTabIndex=0;
- 
- 
-  late List<Widget> pages;
-  late Widget currentPage;
-  late Home homepage;
-  late Profile profile;
-  late Order order;
-  late Wallet wallet;
+  late User _currentUser;  // Firebase User object
+  late String _currentUserUid;  // User's UID
 
+  int currentTabIndex = 0;
+
+  late List<Widget> pages;
+  late HomePage homepage;
+  late LocationsScreen locationsScreen;  // LocationsScreen with required parameters
+  late Order order;
+  late Videoupload videoupload;
 
   @override
   void initState() {
-    homepage=Home();
-    order=Order();
-    profile=Profile();
-    wallet=Wallet();
-    pages=[homepage,order, wallet, profile];
-
-
     super.initState();
+    _fetchCurrentUser();  // Fetch current user when widget initializes
   }
-  
+
+  Future<void> _fetchCurrentUser() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user = auth.currentUser;
+
+    if (user != null) {
+      setState(() {
+        _currentUser = user;
+        _currentUserUid = user.uid;
+        
+        // Initialize pages with the current user's UID
+        homepage = HomePage();
+        order = Order();
+        locationsScreen = LocationsScreen(key: UniqueKey(), currentUserUid: _currentUserUid);
+        videoupload = Videoupload(uid: '',);
+        pages = [homepage, order, videoupload, locationsScreen];
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,26 +59,22 @@ class _BottomNavState extends State<BottomNav> {
         backgroundColor: Colors.white,
         color: Color.fromARGB(255, 2, 65, 237),
         animationDuration: Duration(milliseconds: 500),
-        onTap: (int index){
+        onTap: (int index) {
           setState(() {
-            currentTabIndex=index;
+            currentTabIndex = index;
           });
         },
-        
         items: [
-         Icon(
-          Icons.home_outlined,
-          color: Colors.white,
-         ),
-         Icon(Icons.access_time, color: Colors.white,),
-         Icon(Icons.add_a_photo_outlined, color: Colors.white,),
-         Icon(Icons.person_outline, color: Colors.white,)
-      ]),
+          Icon(
+            Icons.home_outlined,
+            color: Colors.white,
+          ),
+          Icon(Icons.access_time, color: Colors.white),
+          Icon(Icons.add_a_photo_outlined, color: Colors.white),
+          Icon(Icons.person_outline, color: Colors.white),
+        ],
+      ),
       body: pages[currentTabIndex],
-      );
-      
-
-      
-    
+    );
   }
 }
